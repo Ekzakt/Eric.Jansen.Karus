9e3bd184a4f4d9efa1ad2b3ac09f1c330b675ad8
EricJansen <EricJansen@LAPTOP-EJ>
2024-12-20 09:43:21 +0100

    Abstracted away dependency injection.

---
+++ Karus.Application/Configuration/KarusOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Karus.Application.Configuration;
+
+public class KarusOptions
+{
+    public const string SectionName = "Karus";
+
+    public List<string> AllowedIpAddresses { get; set; } = [];
+}
